d488c3918a92d39b58bf0cbc62c88497d87872cf
omarraf <omarrafiqq@gmail.com>
2026-02-20 01:48:09 -0800

    update transition

--- components/page-transition.tsx
+++ components/page-transition.tsx
@@ -15,9 +15,9 @@ export function PageTransition({ children }: PageTransitionProps) {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, x: 20 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -20 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{
           duration: 0.3,
           ease: [0.22, 1, 0.36, 1],
